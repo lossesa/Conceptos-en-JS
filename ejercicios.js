c833064d2502cc2a47789282b6c5d7540a8bd860
sara <sara24>
2025-08-26 21:33:10 -0500

    Actualizacion ejercicios.

---
+++ ejercicios.js
@@ -0,0 +1,27 @@
+// Ejercicio 4
+let numero_uno = 2;
+let numero_dos = 5;
+
+if (numero_uno > numero_dos){
+    console.log(numero_uno, numero)
+}
+
+
+
+
+// Ejercicio 5
+
+// const estrato = 4;
+// if (estrato === 1){
+//     console.log("Estrato bajo.");
+// } else if (estrato === 2){
+//     console.log("Estrato medio bajo.");
+// } else if (estrato === 3){
+//     console.log("Estrato medio.");
+// } else if (estrato === 4){
+//     console.log("Estrato medio alto.")
+// } else if (estrato == 5){
+//     console.log("Estrato alto.")
+// } else {
+//     console.log("Opcion invalida.")
+// }
